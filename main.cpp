79bf77225cf60125279e7ca6f93afbb32eb48ce0
ctake <ctake1235@gmail.com>
2023-08-29 13:30:53 +0900

    change: behaviorof main

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ void reader(void);
 bool InterrupterData[6] = {};
 
 // 0:前 1:中 2:後
-int  passcount[3] = {};   
+bool  passcount[6] = {};   
 
 
 int main(){
@@ -33,24 +33,23 @@ int main(){
         reader();
 
         // 通過チェック
-        for(int i = 0; i < 3; i += 2){
-            if(InterrupterData[i])      passcount[i] += 1;
-            if(InterrupterData[i+1])    passcount[i] += 1;
+        for(int i = 0; i < 6; i++){
+            if(InterrupterData[i] && passcount[i] != 1) passcount[i] = true;
         }
-        if(InterrupterData[0] || InterrupterData[1]){   // 前のセンサどっちかで読んだとき
+        if(passcount[0] || passcount[1]){   // 前のセンサどっちかで読んだとき
             fr_lock .pulsewidth_us(OPEN);
             front   .pulsewidth_us(OPEN);
         }
-        if(InterrupterData[2] || InterrupterData[3]){   // 中のセンサどっちかで読んだとき
-            if(passcount[0] >= 2){
+        if(passcount[2] || passcount[3]){   // 中のセンサどっちかで読んだとき
+            if(passcount[0] && passcount[1]){
                 fr_lock .pulsewidth_us(CLOSE);
                 front   .pulsewidth_us(CLOSE);
                 ThisThread::sleep_for(500ms);    // fr_lockが閉じ切らないうちにfrontが開くのを防ぐ
                 center  .pulsewidth_us(OPEN);
             }
         }
-        if(InterrupterData[4] || InterrupterData[5]){   // 後ろのセンサどっちかで読んだとき
-            if(passcount[1] >= 2){
+        if(passcount[4] || passcount[5]){   // 後ろのセンサどっちかで読んだとき
+            if(passcount[2] && passcount[3]){
                 center  .pulsewidth_us(CLOSE);
                 ThisThread::sleep_for(500ms);   // frontが閉じ切らないうちにcenterが開くのを防ぐ
                 back    .pulsewidth_us(OPEN);
@@ -60,11 +59,11 @@ int main(){
 
         Timer tm;   // 後ろが開いてから閉じるまでの時間を測る
 
-        if(passcount[2] >= 2){
+        if(passcount[4] && passcount[5]){
             if(tm.elapsed_time() >= 5s){
                 back    .pulsewidth_us(CLOSE);
                 bc_lock .pulsewidth_us(CLOSE);
-                for(int i = 0; i < 3; i++)  passcount[i] = 0;
+                for(int i = 0; i < 6; i++)  passcount[i] = false;
             }
         }
 
