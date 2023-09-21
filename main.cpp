2def955c0f7c002f5a8cbef1158f255babf971f0
ctake <ctake1235@gmail.com>
2023-09-21 23:36:39 +0900

    fix: pinmap, param of pulsewidth, and commented out for debug

--- main.cpp
+++ main.cpp
@@ -3,19 +3,19 @@
 
 
 // インタラプタいろいろ
-DigitalIn   PhIn_FR (A0);
-DigitalIn   PhIn_FL (A1);
-DigitalIn   PhIn_CR (A2);
-DigitalIn   PhIn_CL (A3);
-DigitalIn   PhIn_BR (A4);
-DigitalIn   PhIn_BL (A5);
+DigitalIn   PhIn_FR (PA_1);
+DigitalIn   PhIn_FL (PA_3);
+DigitalIn   PhIn_CR (PA_4);
+DigitalIn   PhIn_CL (PA_5);
+DigitalIn   PhIn_BR (PA_6);
+DigitalIn   PhIn_BL (PA_7);
 
 // 各サーボ
-PwmOut      fr_lock (D1);
-PwmOut      front   (D2);
-PwmOut      center  (D3);
-PwmOut      back    (D4);
-PwmOut      bc_lock (D5);
+PwmOut      fr_lock (PB_4);
+PwmOut      front   (PB_5);
+PwmOut      center  (PA_11);
+PwmOut      back    (PB_7);
+PwmOut      bc_lock (PA_12);
 
 // 送電停止
 DigitalOut sig(D2);
@@ -32,8 +32,8 @@ bool  passcount[6] = {};
 // サーボモーターええ感じの角度
 const int   TOUCH   = 850;
 const int   LOSS    = 1700;
-const int   LOCK    = 850;
-const int   FREE    = 1700;
+const int   LOCK    = 1500;
+const int   FREE    = 1000;
 
 int main(){
 
@@ -50,40 +50,40 @@ int main(){
 
         // 通過チェック
         for(int i = 0; i < 6; i++){
-            if(InterrupterData[i] && (passcount[i] == false)) passcount[i] = true;
+            if(!InterrupterData[i]) passcount[i] = true;
         }
         if(passcount[0] || passcount[1]){   // 前のセンサどっちかで読んだとき
             sig     .write(0); // 送電停止
             fr_lock .pulsewidth_us(FREE);
             front   .pulsewidth_us(LOSS);
         }
-        if(passcount[2] || passcount[3]){   // 中のセンサどっちかで読んだとき
+        if(passcount[2] && passcount[3]){   // 中のセンサどっちかで読んだとき
             if(passcount[0] && passcount[1]){
                 fr_lock .pulsewidth_us(LOCK);
                 front   .pulsewidth_us(TOUCH);
-                ThisThread::sleep_for(500ms);    // fr_lockが閉じ切らないうちにfrontが開くのを防ぐ
+                ThisThread::sleep_for(300ms);    // fr_lockが閉じ切らないうちにfrontが開くのを防ぐ
                 center  .pulsewidth_us(LOSS);
             }
         }
-        if(passcount[4] || passcount[5]){   // 後ろのセンサどっちかで読んだとき
+        if(passcount[4] && passcount[5]){   // 後ろのセンサどっちかで読んだとき
             if(passcount[2] && passcount[3]){
                 center  .pulsewidth_us(TOUCH);
-                ThisThread::sleep_for(500ms);   // frontが閉じ切らないうちにcenterが開くのを防ぐ
+                ThisThread::sleep_for(300ms);   // frontが閉じ切らないうちにcenterが開くのを防ぐ
                 back    .pulsewidth_us(LOSS);
                 bc_lock .pulsewidth_us(FREE);
             }
         }
 
-        Timer tm;   // 後ろが開いてから閉じるまでの時間を測る
+        // Timer tm;   // 後ろが開いてから閉じるまでの時間を測る
 
-        while(passcount[4] && passcount[5]){
-            if(tm.elapsed_time() >= 5s){
-                back    .pulsewidth_us(TOUCH);
-                bc_lock .pulsewidth_us(LOCK);
-                sig.write(1);
-                for(int i = 0; i < 6; i++)  passcount[i] = false;
-            }
-        }
+        // while(passcount[4] && passcount[5]){
+        //     if(tm.elapsed_time() >= 5s){
+        //         back    .pulsewidth_us(TOUCH);
+        //         bc_lock .pulsewidth_us(LOCK);
+        //         sig.write(1);
+        //         for(int i = 0; i < 6; i++)  passcount[i] = false;
+        //     }
+        // }
 
     }
     return 0;
@@ -96,4 +96,8 @@ void reader(void){
     InterrupterData[3] = PhIn_CL.read();
     InterrupterData[4] = PhIn_BR.read();
     InterrupterData[5] = PhIn_BL.read();
+	for(int i = 0; i < 6; i++){
+		printf("%d ", InterrupterData[i]);
+	}
+	printf("\n");
 }
